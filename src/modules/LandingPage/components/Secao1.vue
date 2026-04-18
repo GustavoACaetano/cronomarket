023ad0f415d6efb1cf5d7500bc1f2f1e1e833b62
Hei-Lima <heitor.lipeixoto@gmail.com>
2026-04-17 21:45:55 -0300

    [FEAT] insere video na secao 1

--- src/modules/LandingPage/components/Secao1.vue
+++ src/modules/LandingPage/components/Secao1.vue
@@ -5,5 +5,11 @@
             <h1 class="text-6xl text-transparent bg-clip-text bg-linear-to-r from-black via-gray-500 to-gray-400">onde o futuro vira mercado</h1>
             <button class="mt-4 bg-black text-white px-8 py-3 rounded-full text-base font-medium hover:bg-gray-800 transition-colors">Jogar agora!</button>
         </div>
+
+        <video autoplay loop muted class="mx-auto mt-10 rounded-lg w-full max-w-4xl">
+            <source src="/landingvideo.mp4" type="video/mp4">
+            Seu navegador não suporta a tag de vídeo.
+        </video>
+        
     </section>
 </template>
