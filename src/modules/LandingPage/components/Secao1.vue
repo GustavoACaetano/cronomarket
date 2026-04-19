851ddeb707d3299df59d15b53ac45a06416235b9
Davi Henrique Comério <92698311+DaviComerio@users.noreply.github.com>
2026-04-19 15:56:13 -0300

    Fix tamanho hierarquia secao 1

--- src/modules/LandingPage/components/Secao1.vue
+++ src/modules/LandingPage/components/Secao1.vue
@@ -2,9 +2,9 @@
     <section id="home" class="mx-auto text-center animate-fade-in-up" style="opacity: 0; animation-delay: 0.2s">
         <div class="flex flex-col items-center gap-2">
             <h1 class="md:text-7xl text-4xl">Cronomarket</h1>
-            <h2 class="md:text-5xl text-3xl text-transparent bg-clip-text bg-linear-to-r from-black via-gray-500 to-gray-400">onde o futuro vira mercado</h2>
+            <p class="md:text-5xl text-3xl text-transparent bg-clip-text bg-linear-to-r from-black via-gray-500 to-gray-400">onde o futuro vira mercado</p>
 
-            <h3 class="md:text-6xl text-3xl mt-8 text-gray-900">O mercado perfeito para testar seus conhecimentos!</h3>
+            <h2 class="md:text-6xl text-3xl mt-8 text-gray-900">O mercado perfeito para testar seus conhecimentos!</h2>
 
             <button class="mt-8 bg-black text-white px-8 py-3 rounded-full text-base font-medium hover:bg-gray-800 transition-colors">Jogar agora!</button>
         </div>
