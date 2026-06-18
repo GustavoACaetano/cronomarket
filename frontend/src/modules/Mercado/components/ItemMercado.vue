9207320f72cf33507e1b3a119c17adffe941ea69
joaopbarcellos <jpzbarcellos@gmail.com>
2026-06-17 22:21:04 -0300

    Merge branch 'dev' of github.com:GustavoACaetano/cronomarket into dev

--- frontend/src/modules/Mercado/components/ItemMercado.vue
+++ frontend/src/modules/Mercado/components/ItemMercado.vue
@@ -80,18 +80,18 @@ const goToMarket = () => {
             <div class="flex flex-col gap-y-2 pt-1">
                 <!-- Yes Option Line -->
                 <div class="flex items-center text-sm gap-x-3 justify-between">
+                    <span class="text-gray-500 font-light">{{ mercado.opcao_sucesso }}</span>
                     <div class="flex items-center justify-center bg-primary-50 text-primary-700 font-medium px-3 py-1 rounded text-xs min-w-12.5 text-center border border-primary-200">
-                        {{ mercado.opcao_sucesso }}
+                        {{ percentSucesso }}%
                     </div>
-                    <span class="font-semibold text-gray-900">{{ percentSucesso }}%</span>
                 </div>
 
                 <!-- No Option Line -->
                 <div class="flex items-center text-sm gap-x-3 justify-between">
+                    <span class="text-gray-500 font-light">{{ mercado.opcao_fracasso }}</span>
                     <div class="flex items-center justify-center bg-red-50 text-red-700 font-medium px-3 py-1 rounded text-xs min-w-12.5 text-center border border-red-200">
-                        {{ mercado.opcao_fracasso }}
+                        {{ percentFracasso }}%
                     </div>
-                    <span class="font-semibold text-gray-900">{{ percentFracasso }}%</span>
                 </div>
             </div>
 
